f45bc310d5c12e778f2507a13b634d5240978348
Doridian Draconia <mriq91@gmail.com>
2014-06-01 16:07:23 +0200

    de.doridian -> com.foxelbox

--- src/main/java/com/foxelbox/dependencies/config/Configuration.java
+++ src/main/java/com/foxelbox/dependencies/config/Configuration.java
@@ -14,7 +14,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with ConfigDependency.  If not, see <http://www.gnu.org/licenses/>.
  */
-package de.doridian.dependencies.config;
+package com.foxelbox.dependencies.config;
 
 import java.io.*;
 import java.util.HashMap;
